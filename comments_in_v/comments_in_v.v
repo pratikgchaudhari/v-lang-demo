85363298ded4de8c178f706f247f988f1a32e158
Pratik Chaudhari <chaudharipratikg@gmail.com>
2020-02-02 22:43:33 +0530

    Added comments_in_v

---
+++ comments_in_v/comments_in_v.v
@@ -0,0 +1,6 @@
+//This is a single-line comment
+/*
+  This is a multi-line comment.
+  Just adding another line to prove my point.
+*/
+println("The sole purpose of this program is to demonstrate single- and multi-line comments in V!")
