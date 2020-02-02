aef149ff6749ef1880f0a95191e09296d2a516b3
Pratik Chaudhari <chaudharipratikg@gmail.com>
2020-02-02 22:58:38 +0530

    Modified functions_in_v to include 'private' and 'public' functions

--- functions_in_v/functions_in_v.v
+++ functions_in_v/functions_in_v.v
@@ -1,7 +1,13 @@
 fn hey(name string) string {
-	return "Hey, " + name + ".\nI'm a function in V. My name is 'hey()'"
+	return "Hey, " + name + ".\nI'm a function in V.\nMy name is 'hey()', and I'm a 'private' function (which basically means that I'm not invocable from outside this file)"
+}
+
+pub fn hello(name string) string {
+	return "Hey, " + name + ".\nI'm a function in V.\nMy name is 'hello()', and I'm a 'public' function (which basically means that even the code that resides outside this file can invoke me)"
 }
 
 fn main() {
 	println(hey("Sam"))
+	println("")
+	println(hello("Sam"))
 }
